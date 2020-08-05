f6149ffe2248b2e96caf110de4717f39a5d8afa9
hmmhmmhm <hmmhmmhm@naver.com>
2020-08-05 20:17:33 +0900

    Add @types

--- client/components/index.ts
+++ client/components/index.ts
@@ -1,4 +1,2 @@
-// @ts-ignore
 import Card from './Card.vue'
-
 export { Card }
--- client/types/vuex/action.ts
+++ client/types/vuex/action.ts
@@ -1,4 +1,4 @@
-import type { MutationTree, ActionTree, ActionContext } from 'vuex'
+import type { ActionTree, ActionContext } from 'vuex'
 import type { Context as AppContext } from '@nuxt/types'
 
 export interface Actions<S, R> extends ActionTree<S, R> {
