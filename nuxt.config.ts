95d04ca8140d8bdfc83b63060b4a2beb0378a0ac
blackMoja <limlim980625@gmail.com>
2020-08-05 16:50:10 +0900

    [settings]
     - tailwind build issue
     - unused tailwind css remove options add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+const path = require('path');
+
 export default {
     srcDir: './client',
     env: {},
@@ -19,14 +21,29 @@ export default {
     },
     loading: { color: '#3B8070' },
     css: ['~/assets/css/main.css'],
-    build: {},
+    build: {
+        postcss: {
+            plugins: {
+                'postcss-import': {},
+                tailwindcss: path.resolve(__dirname, './tailwind.config.js'),
+                'postcss-nested': {}
+            }
+        },
+        preset: {
+            stage: 1
+        }
+    },
     buildModules: ['@nuxt/typescript-build', '@nuxtjs/tailwindcss'],
     tailwindcss: {
         configPath: '~/config/tailwind.config.js',
         cssPath: '~/assets/css/tailwind.css',
         exposeConfieg: false
     },
-    modules: ['@nuxtjs/axios'],
+    modules: ['@nuxtjs/axios', 'nuxt-purgecss'],
+    purgeCSS: {
+        mode: 'postcss',
+        enabled: (process.env.NODE_ENV === 'production')
+    },
     axios: {},
     plugins: ['~/plugins/composition-api']
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	purge: {
+		enabled: true,
+		content: [
+			'./src/**/*.html',
+    		'./src/**/*.vue',
+    		'./src/**/*.jsx',
+		],
+	  },
+	theme: {
+		extend: {}
+	},
+	variants: {},
+	plugins: []
+};
